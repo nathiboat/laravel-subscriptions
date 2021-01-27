f246a668110dcb9d1bde2049c72e63910609d352
Nathi Taengnim <nathi.t@outlook.com>
2021-01-27 16:56:12 +0000

    add sucscription features

---
+++ app/Http/Controllers/Subscriptions/SubscriptionController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Subscriptions;
+
+use App\Http\Controllers\Controller;
+use App\Models\Plan;
+use Illuminate\Http\Request;
+
+class SubscriptionController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+
+    public function index(Request $request)
+    {
+        return view('subscriptions.checkout', [
+            'intent' => $request->user()->createSetupIntent()
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'token' => 'required'
+        ]);
+
+
+        $plan = Plan::where('slug', $request->plan)
+            ->orWhere('slug', 'monthly')
+            ->first();
+
+        $request->user()->newSubscription('default', $plan->stripe_id)
+            ->create($request->token);
+
+        return back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Subscriptions\PlanController;
+use App\Http\Controllers\Subscriptions\SubscriptionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,5 +24,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['namespace' => 'Subscriptions'], function () {
-    Route::get('/plans', [PlanController::class, 'index'])->name('plans');
+    Route::get('/plans', [PlanController::class, 'index'])->name('subscriptions.plans');
+    Route::get('/subscriptions', [SubscriptionController::class, 'index'])->name('subscriptions');
+    Route::post('/subscriptions', [SubscriptionController::class, 'store'])->name('subscriptions.store');
 });
