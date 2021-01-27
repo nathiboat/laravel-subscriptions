7c9e2d16a108236f0b73e78c6bff6deca99b7168
Nathi Taengnim <nathi.t@outlook.com>
2021-01-27 02:03:50 +0000

    adding plan routes and controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Subscriptions\PlanController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['namespace' => 'Subscriptions'], function () {
+    Route::get('/plans', [PlanController::class, 'index'])->name('plans');
+});
